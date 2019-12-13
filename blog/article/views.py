69b1b056a2e80ea3d5ab5206bdf083a934e67f90
temo881030 <temo881030@gmail.com>
2019-12-13 16:25:32 +0800

    1213

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+
 
 from article.models import Article, Comment
 from article.forms import ArticleForm
@@ -30,4 +32,5 @@ def articleCreate(request):
         return render(request, template, {'articleForm':articleForm})
 
     articleForm.save()
-    return article(request)
+    messages.success(request, '文章已新增')
+    return redirect('article:article')
